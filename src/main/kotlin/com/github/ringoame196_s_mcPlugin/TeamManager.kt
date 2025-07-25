0a3e074008f951ea3c1df637f6718b96be09afa4
りんご飴196 <132573268+Ringoame196@users.noreply.github.com>
2025-07-26 01:52:52 +0900

    ⬆️ チーム色とディスプレイ名を表示

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamManager.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/TeamManager.kt
@@ -1,11 +1,14 @@
 package com.github.ringoame196_s_mcPlugin
 
+import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 object TeamManager {
     fun getTeamName(player: Player): String {
         val team = player.scoreboard.getEntryTeam(player.name)
         val teamName = team?.name ?: "参加していません"
-        return teamName
+        val teamDisplayName = team?.displayName ?: teamName
+        val teamColor = team?.color ?: ChatColor.WHITE
+        return "${teamColor}$teamDisplayName"
     }
 }
